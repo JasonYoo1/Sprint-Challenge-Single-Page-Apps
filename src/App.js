93565d413979c4ed47218287cbe3b9c470d1eefa
JasonYoo1 <jyoojs@gmail.com>
2019-07-26 10:44:36 -0700

    made NavLink to home

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React from 'react';
 import TabNav from './components/TabNav.js';
 import Header from './components/Header.js';
 import AppRouter from './components/AppRouter.js';
+import { NavLink } from 'react-router-dom'
+
 
 export default function App() {
   return <main>
     <Header />
+    <NavLink to="/">Home</NavLink>
     <TabNav />
     <AppRouter />
   </main>
